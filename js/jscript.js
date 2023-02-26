452a8e52faef8535508f297858cebcfe5491478a
merickagustin <merickic.agustin@gmail.com>
2023-02-26 21:19:31 +0800

    New Update for testimonial and meals section

--- js/jscript.js
+++ js/jscript.js
@@ -1,4 +1,4 @@
-/**********************Mobile NAVIGATION********************/
+/**********************MOBILE NAVIGATION********************/
 const menuNavEl = document.querySelector(".btn-mobile-nav");
 
 menuNavEl.addEventListener("click", function () {
@@ -14,4 +14,41 @@ menuNavEl.addEventListener("click", function () {
       childrenEl[i].name = "menu-outline";
     }
   }
+});
+
+/**********************TESTIMONIAL CAROUSEL***************************/
+const btnSlidesEl = document.querySelectorAll(".btn-slide");
+
+const slidesEl = document.querySelector(".slider-container");
+
+btnSlidesEl.forEach((btnSlides) => {
+  btnSlides.addEventListener("click", function () {
+    const self = this;
+    const activeSlide = slidesEl.querySelector(".active-slide");
+    const curIndex = [...slidesEl.children].indexOf(activeSlide);
+    const lastIndex = slidesEl.children.length - 1;
+
+    let nextIndex = 0;
+
+    //Check if the trigger is the next button
+    if (self.className.match("btn-slide--next")) {
+      nextIndex = curIndex !== 0 ? curIndex - 1 : lastIndex;
+
+      //Remove the class to the current slide
+      slidesEl.children[curIndex].classList.remove("active-slide");
+
+      //Assigned the class to the next slide
+      slidesEl.children[nextIndex].classList.add("active-slide");
+    }
+    //otherwise check if the trigger is the prev button
+    else if (self.className.match("btn-slide--prev")) {
+      nextIndex = curIndex !== lastIndex ? curIndex + 1 : 0;
+
+      //Remove the class to the current slide
+      slidesEl.children[curIndex].classList.toggle("active-slide");
+
+      //Assigned the class to the next slide
+      slidesEl.children[nextIndex].classList.toggle("active-slide");
+    }
+  });
 });
